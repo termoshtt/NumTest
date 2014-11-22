7b7b190b057853df490f85077dcacb4d69894508
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-11-23 00:02:43 +0900

    Add definition of Test class

--- Test.hpp
+++ Test.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <mutex>
+
 /*!
  * @namespace NumTest
  * @brief test library for Numerical Simulation
@@ -25,8 +28,32 @@
  */
 namespace NumTest {
 
-class Test;
+class Test {
+  std::string name, desc;
+  double eps;
+  size_t count;
 
-class SubTest;
+public:
+  Test(const char *name /** identifier of test */,
+       const char *desc = "" /** description of test */,
+       double eps = 1e-14 /** acceptable error */)
+      : name(name), desc(desc), eps(eps), count(1) {}
+
+  /** reset filename of the result XML */
+  void reset_filename(const char *filename);
+
+  /** reset acceptable error */
+  void reset_eps(double eps_) { eps = eps_; }
+  /** reset description */
+  void reset_desc(std::string desc_) { desc = desc_; };
+  /** get current description */
+  std::string get_desc() const { return desc; }
+
+  /** test two values are equal */
+  template <typename T> void equal(T val, T ans);
+  /** test two ranges are equal */
+  template <typename Range>
+  void range_equal(const Range &val, const Range 6ans);
+};
 
 } // namespace NumTest
